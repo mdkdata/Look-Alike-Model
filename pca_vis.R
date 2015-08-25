b52915b4ea6eb9dfc4d6d6abd2544ae50c5aabe1
ivanliu1989 <ivan.liuyanfeng@gmail.com>
2015-08-25 20:49:09 +1000

    update

--- pca_vis.R
+++ pca_vis.R
@@ -8,9 +8,13 @@ dim(fco_dt);dim(llo_dt)
 ### Feature Selection ###
 #########################
 # round(cor(fco_dt[,1:4]), 2)
+# cols <-
+#     colnames(fco_dt)[c(
+#         1,3,5,6,7,8,21:28,30:90,124:139,140,143,146:150,152,154:155,160:163,167:175,177:204,206:207
+#     )]
 cols <-
     colnames(fco_dt)[c(
-        1,3,5,6,7,8,21:28,30:90,124:139,140,143,146:150,152,154:155,160:163,167:175,177:204,206:207
+        1,2,5,6,8,21:28,124:139,140:141,152,154:155,160:163,172:175,177:204,206:207
     )]
 
 fco <- fco_dt[,cols]
@@ -18,12 +22,12 @@ llo <- llo_dt[,cols]
 #str(fco[,99:147])
 fco[,c('LOYALTY_RANK_NUMBER')] <-
     as.factor(fco[,c('LOYALTY_RANK_NUMBER')])
-fco[,c('MBR_RES_PCODE')] <- as.factor(fco[,c('MBR_RES_PCODE')])
+# fco[,c('MBR_RES_PCODE')] <- as.factor(fco[,c('MBR_RES_PCODE')])
 fco$target <- 1
 
 llo[,c('LOYALTY_RANK_NUMBER')] <-
     as.factor(llo[,c('LOYALTY_RANK_NUMBER')])
-llo[,c('MBR_RES_PCODE')] <- as.factor(llo[,c('MBR_RES_PCODE')])
+# llo[,c('MBR_RES_PCODE')] <- as.factor(llo[,c('MBR_RES_PCODE')])
 llo$target <- 0
 
 ######################
@@ -31,9 +35,11 @@ llo$target <- 0
 ######################
 library(caret)
 all_df <- rbind(fco,llo)
+dim(all_df)
 dummies <- dummyVars(target ~ ., data = all_df)
 all_dum <- predict(dummies, newdata = all_df)
 
+all_dum <- all_dum[,-c(3,4,7,16,17)]
 # for (i in 1:ncol(fco)) {
 #     if (is.factor(fco[,i])) {
 #         print(i)
@@ -56,7 +62,7 @@ all_dum_nzv <- all_dum[, -nzv]
 ###########
 pc <-
     princomp(
-        all_dum_nzv[,2:(ncol(all_dum_nzv) - 1)], cor = TRUE, scores = TRUE, center = TRUE,
+        all_dum[,2:(ncol(all_dum) - 1)], cor = TRUE, scores = TRUE, center = TRUE,
         scale. = TRUE
     )
 summary(pc)
