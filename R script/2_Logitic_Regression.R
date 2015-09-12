76fa362dff4c30b2d5c846e35774cbe6db64c61d
ivanliu1989 <ivan.liuyanfeng@gmail.com>
2015-09-13 09:31:12 +1000

    random forest

--- R script/2_Logitic_Regression.R
+++ R script/2_Logitic_Regression.R
@@ -22,16 +22,32 @@ fitControl <- trainControl(method = "cv",
                            number = 10,
                            classProbs = TRUE,
                            summaryFunction = twoClassSummary)
-Grid <-  expand.grid(n.trees = 150, interaction.depth = 6, shrinkage = 0.01)
+#Grid <-  expand.grid(n.trees = 150, interaction.depth = 6, shrinkage = 0.02)
+Grid <-  expand.grid(mtry=6)
+
 # Training
 set.seed(825)
 fit <- train(FCO_CST ~ ., data=train[,-1],
-                   method = "gbm",
+                   method = "rf",
                    trControl = fitControl,
                    tuneGrid = Grid,
                    preProcess = c('center', 'scale'),
                    metric ='ROC',
                    verbose = T)
+
+# Training on Full set
+fitControl2 <- trainControl(method = "none",
+                           classProbs = TRUE,
+                           summaryFunction = twoClassSummary)
+set.seed(825)
+fit2 <- train(FCO_CST ~ ., data=train[,-1],
+             method = "rf",
+             trControl = fitControl,
+             tuneGrid = Grid,
+             preProcess = c('center', 'scale'),
+             metric ='ROC',
+             verbose = T)
+
 
 # Plot
 trellis.par.set(caretTheme())
@@ -48,5 +64,12 @@ for(i in 1:ncol(test)){
   if(i %in% c(2,3,5:17)) test[,i] <- as.factor(test[,i])
 }
 p <- predict(fit, newdata = test[,-c(1,38)],type = "prob")
+pred <- cbind(test[,1], p)
+names(pred) <- c('Mbr_Num', 'N', 'Y')
+
+# Output
 head(p)
-length(p[which(p$Y>=.25),2])
+length(p[which(p$Y>=.5),2])
+
+fit_rf <- fit
+save(fit_rf, pred, file='20150913_lookalike_results_rf.RData')
